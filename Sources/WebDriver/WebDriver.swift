cf036577821336e0421820e8d9c9d2ab08edff01
ntflix <felix@iron59.co.uk>
2025-11-05 13:21:19 +0000

    Begin async implementation

--- Sources/WebDriver/WebDriver.swift
+++ Sources/WebDriver/WebDriver.swift
@@ -5,7 +5,7 @@ public protocol WebDriver: Sendable {
     /// Sends a WebDriver request to the server and returns the response.
     /// - Parameter request: The request to send.
     @discardableResult
-    func send<Req: Request>(_ request: Req) throws -> Req.Response
+    func send<Req: Request>(_ request: Req) async throws -> Req.Response
 
     /// Determines if a given error is inconclusive and should be retried.
     func isInconclusiveInteraction(error: ErrorResponse.Status) -> Bool
@@ -15,10 +15,10 @@ extension WebDriver {
     /// status - returns WinAppDriver status
     /// Returns: an instance of the WebDriverStatus type
     public var status: WebDriverStatus {
-        get throws {
+        get async throws {
             switch wireProtocol {
-                case .legacySelenium: return try send(Requests.LegacySelenium.Status())
-                case .w3c: return try send(Requests.W3C.Status()).value
+            case .legacySelenium: return try await send(Requests.LegacySelenium.Status())
+            case .w3c: return try await send(Requests.W3C.Status()).value
             }
         }
     }
